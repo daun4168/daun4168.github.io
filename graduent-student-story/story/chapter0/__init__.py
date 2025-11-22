9d4fa23db6cd9e47b95408c5a9f13a8bcc3b027b
정다운[Auto Targeting] <daun.jeong@navercorp.com>
2025-11-22 13:07:27 +0900

    change filenames

--- graduent-student-story/story/chapter0/__init__.py
+++ graduent-student-story/story/chapter0/__init__.py
@@ -1,6 +1,6 @@
-from .scene0 import CH0_SCENE0_DATA
-from .scene1 import CH0_SCENE1_DATA
-from .scene2 import CH0_SCENE2_DATA
+from .ch0scene0 import CH0_SCENE0_DATA
+from .ch0scene1 import CH0_SCENE1_DATA
+from .ch0scene2 import CH0_SCENE2_DATA
 
 __all__ = [
     "CH0_SCENE0_DATA",
--- graduent-student-story/story/chapter1/__init__.py
+++ graduent-student-story/story/chapter1/__init__.py
@@ -1,8 +1,8 @@
-from .common import CH1_COMMON_DATA
-from .scene0 import CH1_SCENE0_DATA
-from .scene1 import CH1_SCENE1_DATA
-from .scene2 import CH1_SCENE2_DATA
-from .scene3 import CH1_SCENE3_DATA
+from .ch1common import CH1_COMMON_DATA
+from .ch1scene0 import CH1_SCENE0_DATA
+from .ch1scene1 import CH1_SCENE1_DATA
+from .ch1scene2 import CH1_SCENE2_DATA
+from .ch1scene3 import CH1_SCENE3_DATA
 
 __all__ = [
     "CH1_COMMON_DATA",
